0c9faf096335a44cd04daa2459c9365638f1da60
Rafael Campos da Silva <110067506+Rafaelcds21@users.noreply.github.com>
2022-11-07 12:26:23 -0300

    Create APDVOtica_lentes.js

---
+++ APDVOtica/APDVOtica_lentes.js
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>questão 1</title>
+    <link rel="stylesheet" type="text/css" href="questão 1.css" media="screen" />
+</head>
+<body>
+    <script>
+      function voltar(){
+          window.location.href = "file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/quest%C3%A3o%201.html";
+      }
+    </script>
+
+    <img src="C:\Users\Rafael Campos\OneDrive\Área de Trabalho\projetos js\constr. de pag. web\AD2\APDVOtica_LogoTopo.png" class="imgLogo">
+    
+    <address class="ender">
+        <p>Avenida Parcelada, 123</p>
+        <p>Tel: (21) 91234-5678</p>
+        <p><u>contato@apdv.com.br</u></p>
+    </address>
+
+    <hr color="#FFEE66" width="100%">
+
+    <nav class="menu">
+        <div align="center">
+            <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/pginicial.html" class="botao">inicial</a>
+
+            <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/quest%C3%A3o%201.html" class="botao">lentes</a>
+
+            <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/quest%C3%A3o%202.html" class="botao">armações</a>
+            
+            <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/quest%C3%A3o%204.html" class="botao">compras</a>
+        </div>
+    </nav>
+
+    <hr color="#FFEE66">
+
+    <p style="font-size: 21px;">Lentes de contato reutilizáveis</p>
+
+    <p>todas em 10x sem juros no cartão</p>
+    
+    <table border="0" width="100%">
+        <td>
+            <ol class=listaLentes>
+                <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/softlens.html" style="color: #550099;">
+                    <li>
+                        <p>
+                            SoftLens
+                        </p>
+                    </li>
+                </a>
+
+                <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/bioinfinity.html" style="color: #550099;">
+                    <li>
+                        <p>
+                            Bioinfinity
+                        </p>
+                    </li>
+                </a>
+
+                <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/acuvue%20oasys.html" style="color: #550099;">
+                    <li>
+                        <p>
+                            Acuvue Oasys
+                        </p>
+                    </li>
+                </a>
+
+                <a href="file:///C:/Users/Rafael%20Campos/OneDrive/%C3%81rea%20de%20Trabalho/projetos%20js/constr.%20de%20pag.%20web/AD2/acuvue%202.html" style="color: #550099;">
+                    <li>
+                        <p>
+                            Acuvue 2
+                        </p>
+                    </li>
+                </a>
+            </ol>
+        </td>
+    </table>
+</body>
+</html>
